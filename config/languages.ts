adb2af17ea86c641342e0afbdaf8229c7e94c4dc
Jacob Garcia <jagarcia1651@gmail.com>
2025-01-08 19:59:51 -0700

    Remove unused code.

--- config/languages.ts
+++
@@ -1,8 +0,0 @@
-export interface Language {
-  code: string;
-  name: string;
-}
-
-export const AVAILABLE_LANGUAGES: Language[] = [
-  { code: 'en', name: 'English' }
-]; 
--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  output: 'export',
-  basePath: "/gardengemswebsite",
+  output: 'export'
 }
 
 module.exports = nextConfig 
